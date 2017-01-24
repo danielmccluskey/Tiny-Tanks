4814827bb78117cf84c2efea8fc6112183db9cbc
Daniel McCluskey <dmccluskey98+Uni@gmail.com>
2017-01-24 03:49:16 +0000

    Added generic variables to Main.cpp
    Added mathLibrary.h with some basic math functions.

--- source/main.cpp
+++ source/main.cpp
@@ -4,17 +4,26 @@
 #include <sstream>
 #include <iomanip>
 
+//Screen sizes
+int iScreenWidth = 1280;
+int iScreenHeight = 720;
+
+//Co-ords of the Center of the screen
+float fCenterX = iScreenWidth*0.5f;
+float fCenterY = iScreenHeight*0.5f;
 
 int main(int argv, char* argc[])
 {
 	
-	if (UG::Create(1024, 768, false, "Pong", 100, 100))
+	if (UG::Create(iScreenWidth, iScreenHeight, false, "Pong", 100, 100))
 	{
 		
 		UG::SetBackgroundColor(UG::SColour(0x2A, 0x57, 0x66, 0xFF));
 		UG::AddFont("./fonts/invaders.fnt");
 		
-
+		int iSpriteID = UG::CreateSprite("./imagesball.png", 100, 100, true);//Create the sprite
+		UG::DrawSprite(iSpriteID);	//Draws it
+		UG::MoveSprite(iSpriteID, fCenterX, fCenterY); //Moves Pacman to starting Position
 
 
 		do 
