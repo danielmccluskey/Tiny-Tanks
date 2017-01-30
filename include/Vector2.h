2956244c1fd27d9cef8ca0346a222b104474dbb3
Daniel McCluskey <dmccluskey98+Uni@gmail.com>
2017-01-30 03:07:55 +0000

    Moved function bodies for Vector2 from Header file to CPP file.

--- include/Vector2.h
+++ include/Vector2.h
@@ -2,39 +2,18 @@
 class Vector2
 {
 public:
-	
-	Vector2::Vector2()
-	{
-		dX = 0.f;
-		dY = 0.f;
-	}
-
-	Vector2(double a_dX, double a_dY)
-	{
-		dX = a_dX;
-		dY = a_dY;
-	}
-
-	bool operator==(const Vector2 &a_v1)
-	{
-		return (a_v1.dX == dX && a_v1.dY == dY);
-	}
-	bool operator!=(const Vector2 &a_v1)
-	{
-		return (a_v1.dX != dX && a_v1.dY != dY);
-	}
-	void operator+=(const Vector2 &a_v1)
-	{
-		dX += a_v1.dX;
-		dY += a_v1.dY;
-	}
-	void operator-(const Vector2 &a_v1)
-	{
-		dX -= a_v1.dX;
-		dY -= a_v1.dY;
-	}
 	double dX;
 	double dY;
+
+	Vector2::Vector2();
+	Vector2(double a_dX, double a_dY);	
+
+	bool operator==(const Vector2 &a_v1);	
+	bool operator!=(const Vector2 &a_v1);	
+	void operator+=(const Vector2 &a_v1);
+	void operator-=(const Vector2 &a_v1);
+	double operator+(const Vector2 &a_v1);
+	double operator-(const Vector2 &a_v1);
 	 
 private:
 	
